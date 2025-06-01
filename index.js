15b52b68e3d2a4160d30ecd54c6921eb2216434f
LyricsBRCK <eliandvl21@gmail.com>
2025-05-31 23:51:29 -0300

    Agregado de sector Quienes somos y Clientes

--- index.js
+++ index.js
@@ -1,39 +1,67 @@
-/* Ocultar/Mostrar menu
-const menu = document.getElementById("menu");
-const linkMenu = document.getElementById("button-menu");
-const desplegable = document.getElementById("menu-desplegable");
+//CARRUSEL
+const images = document.querySelectorAll('.imagenes_carrusel');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+let current = 0;
+let interval = setInterval(nextImage, 4000);
 
-menu.addEventListener("click", () =>
+function showImage(index)
 {
-    if(desplegable.style.display == "flex")
+  images.forEach((img, i) =>
     {
-        desplegable.style.display = "none";
-        linkMenu.style.filter = "invert(0%)";
-    }
-    else
-    {
-        desplegable.style.display = "flex";
-        desplegable.style.flexDirection = "column";
-        desplegable.style.justifyContent = "center";
-        linkMenu.style.filter = "invert(100%)";
-        menuAbierto = true;
-    }
+        img.classList.toggle('active', i === index);
+    });
+}
+
+function nextImage()
+{
+    current = (current + 1) % images.length;
+    showImage(current);
+}
+
+function prevImage() 
+{
+    current = (current - 1 + images.length) % images.length;
+    showImage(current);
+}
+
+function resetInterval() 
+{
+    clearInterval(interval);
+    interval = setInterval(nextImage, 4000);
+}
+
+nextBtn.addEventListener('click', () => 
+{
+    nextImage();
+    resetInterval();
 });
 
-document.addEventListener("click", (e) =>
+prevBtn.addEventListener('click', () => 
 {
-    if(!menu.contains(e.target))
-    {
-        desplegable.style.display = "none";
-        linkMenu.style.filter = "invert(0%)";
-    }
-})
-*/
+    prevImage();
+    resetInterval();
+});
 
+
+//CATEGORIAS
 const categorias = document.querySelectorAll(".imagen_wrapper img");
-let primer_expandir = document.getElementById("primer_expandir");
-let segundo_expandir = document.getElementById("segundo_expandir");
-let tercer_expandir = document.getElementById("container_expandir_derecha");
+
+const primer_expandir = document.getElementById("primer_expandir");
+const segundo_expandir = document.getElementById("segundo_expandir");
+const tercer_expandir = document.getElementById("container_expandir_derecha");
+const conjunto_expandir = [primer_expandir, segundo_expandir, tercer_expandir];
+const primera_lamina = document.getElementById("primera_lamina");
+const segunda_lamina = document.getElementById("segunda_lamina");
+const tercera_lamina = document.getElementById("tercera_lamina");
+const conjunto_laminas = [primera_lamina, segunda_lamina, tercera_lamina];
+
+
+const container_primera_imagen = document.getElementById("container_primera_imagen");
+const container_segunda_imagen = document.getElementById("container_segunda_imagen");
+const container_tercera_imagen = document.getElementById("container_tercera_imagen");
+const containers = [container_primera_imagen, container_segunda_imagen, container_tercera_imagen];
+
 const cruzes = document.querySelectorAll(".cruz");
 
 categorias.forEach(imagen => 
@@ -43,13 +71,19 @@ categorias.forEach(imagen =>
         switch (imagen.id)
         {
             case "primera_imagen":
-                primer_expandir.style.display = "block";
+                resaltarDiv(primer_expandir);
+                cambiarZindex(container_primera_imagen);
+                ocultarLamina(primera_lamina)
                 break;
             case "segunda_imagen":
-                segundo_expandir.style.display = "block";
+                resaltarDiv(segundo_expandir);
+                cambiarZindex(container_segunda_imagen);
+                ocultarLamina(segunda_lamina)
                 break;
-            case "tercera_imagen":
-                tercer_expandir.style.display = "block";
+                case "tercera_imagen":    
+                resaltarDiv(tercer_expandir);
+                cambiarZindex(container_tercera_imagen);
+                ocultarLamina(tercera_lamina)
                 break;
         }
     }); 
@@ -59,9 +93,56 @@ cruzes.forEach(cruz =>
 {
     cruz.addEventListener("click", () =>
     {
-        primer_expandir.style.display = "none";
-        segundo_expandir.style.display = "none";
-        tercer_expandir.style.display = "none";
+        resaltarDiv(null);
+        cambiarZindex(null);
+        ocultarLamina(null)  
     });
 
 });
+
+//FUNCIONES
+
+function ocultarLamina(lamina_ocultar)
+{
+    conjunto_laminas.forEach(lamina => 
+    {
+        if (lamina === lamina_ocultar)
+        {
+            lamina.style.display = "none";
+        }
+        else
+        {
+            lamina.style.display = "flex";
+        }
+    });
+};
+
+function resaltarDiv(div)
+{
+    conjunto_expandir.forEach(container =>
+    {
+        if(container === div)
+        {
+            container.style.display = "block";
+        }
+        else
+        {
+            container.style.display = "none";
+        }
+    });
+}
+
+function cambiarZindex(imagen) 
+{
+    containers.forEach(container => 
+    {
+        if (container === imagen) 
+        {
+            container.style.zIndex = 25;
+        }
+        else
+        {
+            container.style.zIndex = 10;
+        }
+    });
+};
